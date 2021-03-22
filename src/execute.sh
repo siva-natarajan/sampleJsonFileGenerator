7663b83fd67bbe849c4ec8fba085ae3bed1bc92d
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-03-22 06:29:26 +0000

    modified shell script

--- src/execute.sh
+++ src/execute.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-set -e
-
+# set -e
 
 # node ./src/createJson.js
-echo "hello world" > src/newFile.txt
+# echo "hello world" >src/newFile.txt
+# IFS=
+# IFS=$'\n'
+# readContent=$(<src/newFile.txt)
+# echo "$readContent"
+cp src/untested.json src/newFile.json
+
+# coverageReportMessage=$(curl -H "Accept: application/json" -H "Content-Type:
+# application/json" -X POST -d '{"resultDescription":"how are you", "channel":
+# "@sivakumar", "buildNumber":"'"${readContent}"'"}'
+# https://731e8c31-f578-4b67-b5cb-e2f17dee6e50.trayapp.io)
+# coverageReportMessage=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d '{"resultDescription":"$", "channel": "@sivakumar", "buildNumber":"123"}' https://731e8c31-f578-4b67-b5cb-e2f17dee6e50.trayapp.io)
+# curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d '{"resultDescription":"how are you", "channel": "@sivakumar", "coverageFileLink":"www.google.com"}' https://731e8c31-f578-4b67-b5cb-e2f17dee6e50.trayapp.io
